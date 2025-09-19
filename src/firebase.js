ed5d693eaf5e6f3113809b7d63b7014206c7991a
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-09-19 11:24:57 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDpd0bWUEkmjdE-GY5Sp1stnZ0kNA0vTxY",
-  authDomain: "comforty-app-1ccfa.firebaseapp.com",
-  projectId: "comforty-app-1ccfa",
-  storageBucket: "comforty-app-1ccfa.firebasestorage.app",
-  messagingSenderId: "22337789271",
-  appId: "1:22337789271:web:95747536b903b28f50b212",
-  measurementId: "G-Q3DS12JJNY"
+  apiKey: "-----",
+  authDomain: "------",
+  projectId: "-----",
+  storageBucket: "-------",
+  messagingSenderId: "--------",
+  appId: "-------",
+  measurementId: "-------"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
